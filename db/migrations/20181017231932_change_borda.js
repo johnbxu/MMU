c6945d5b314aeea0ded4336844898d919c78cfa1
altanu <altanunsal@gmail.com>
2018-10-17 23:22:14 +0000

    new migration to correct db

---
+++ db/migrations/20181017231932_change_borda.js
@@ -0,0 +1,19 @@
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.table('response', function(table){
+      table.dropColumn('borda');
+    }).table('response', function(table){
+      table.integer('borda');
+    })
+  ])
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.table('response', function(table){
+      table.dropColumn('borda');
+    }).table('response', function(table){
+      table.string('borda');
+    })
+  ])
+};
