7c4889b7e94cb6a3c6ae67391f2e4777af351369
altanu <altanunsal@gmail.com>
2018-10-17 17:37:34 +0000

    first migration to create empty tables

--- db/migrations/20181017164932_initialize_tables.js
+++ db/migrations/20181017164932_initialize_tables.js
@@ -1,8 +1,42 @@
-
 exports.up = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.createTable('poll', function(table){
+      table.integer('id');
+      table.string('text');
+      table.string('creator_email');
+      table.date('date_created');
+      table.date('end_date');
+      table.string('randomURL');
+    }).createTable('response', function(table){
+      table.integer('id');
+      table.integer('poll_id');
+      table.string('text');
+      table.string('borda');
+    }).createTable('voter', function(table){
+      table.integer('id');
+      table.string('name');
+      table.string('email');
+    }).createTable('vote', function(table){
+      table.integer('id');
+      table.integer('response_id');
+      table.integer('voter_id');
+      table.integer('bordaValue');
+    }).createTable('participant', function(table){
+      table.integer('id');
+      table.integer('poll_id');
+      table.integer('voter_id');
+      table.integer('voteStatus');
+    })
+  ])
 };
 
 exports.down = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.dropTable('response')
+    .dropTable('poll')
+    .dropTable('participant')
+    .dropTable('vote')
+    .dropTable('voter')
+
+  ])
 };
