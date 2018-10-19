b91e28dfe3bc956916c0487d72e2d10cad31bf2e
altanu <altanunsal@gmail.com>
2018-10-19 14:48:51 +0000

    popped stashes

---
+++ db/migrations/20181019142820_add_creator_name.js
@@ -0,0 +1,11 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.table('poll', function(table) {
+     table.string("creator_name");
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.table('poll', function(table) {
+    table.dropColumn('creator_name');
+  })
+};
