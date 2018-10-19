b89d23b6b19abc77eb2104d444158b7713db9f6a
John Xu <johnbxu@gmail.com>
2018-10-19 18:37:58 +0000

    made migration to kill voter table

---
+++ db/migrations/20181019181535_axe_voter.js
@@ -0,0 +1,23 @@
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.table('vote', function(table) {
+      table.dropForeign('voter_id');
+      table.dropColumn('voter_id')
+    }),
+    knex.schema.dropTable('voter')
+  ])
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('voter', function(table){
+      table.increments('id');
+      table.string('name');
+      table.string('email');
+    }),
+    knex.schema.table('vote', function(table) {
+      table.integer('voter_id')
+      table.foreign('voter_id').references('voter.id')
+    }),
+  ]);
+}
