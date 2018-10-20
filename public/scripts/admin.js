14848893a22cbe5f028c9d5c7a78c876b89d790d
John Xu <johnbxu@gmail.com>
2018-10-20 16:24:19 +0000

    fixed delete one response button

--- public/scripts/admin.js
+++ public/scripts/admin.js
@@ -1,24 +1,48 @@
 $(() => {
-  $('#submitOrder').click(function (event) {
-    event.preventDefault();
-    const order = []
-    const id = $('#submitOrder').attr('url');
-    Array.from($('#sortable')
-      .children('.sortableContainer'))
-      .forEach((child) => {
-        order.push(Number(child.id));
-      });
-    console.log(order);
-    console.log(id);
-    $.ajax({
-      url: `/polls/${id}`,
-      method: 'PUT',
-      data: {obj: order},
-      dataType: JSON,
-      success: function(result) {
-        console.log(result);
-      }
-    })
-  });
+  $(".deleteResponse").click(function (event) {
+    const endpoint = $(this).attr("url");
+    const responseId = {
+      id: $(this).attr("responseId")
+    }
 
+    $.ajax({
+      url: endpoint,
+      method: "DELETE",
+      data: responseId
+    }).done(function (response) {
+      console.log(response);
+    })
+  })
+
+  // $("#updatePoll").click(function (event) {
+	// 	event.preventDefault();
+	// 	const formVariables = {};
+	// 	formVariables["options"] = [];
+	// 	let temporaryInputArray = [];
+  //
+	// 	$("#responses .option :input").each(function(){
+	// 		temporaryInputArray.push($(this).val());
+	// 	});
+  //
+	// 	for(let i = 0; i < temporaryInputArray.length; i += 2) {
+	// 		let tempObj = {};
+	// 		tempObj["response"] = temporaryInputArray[i];
+	// 		tempObj["description"] = temporaryInputArray[i+1];
+	// 		formVariables.options.push(tempObj);
+	// 	}
+  //
+	// 	formVariables.question = $("#question").val();
+	// 	formVariables.name = $("#name").val();
+	// 	formVariables.email = $("#email").val();
+	// 	formVariables.end = new Date($("#end").val()).toISOString();
+  //
+	// 	$.ajax({
+	// 		url: "/polls/new",
+	// 		method: "POST",
+	// 		data: formVariables,
+	// 		dataType: "json"
+	// 	}).done(function(response) {
+	// 		window.location.href = "http://localhost:8080"+response.url;
+	// 	});
+	// });
 });
