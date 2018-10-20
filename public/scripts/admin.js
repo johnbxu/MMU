3532a145793349a844b34077ee3e90fc8483ba5d
altanu <altanunsal@gmail.com>
2018-10-20 20:02:50 +0000

    merging downstream

--- public/scripts/admin.js
+++ public/scripts/admin.js
@@ -1,10 +1,10 @@
 $(() => {
+  const randomURL = $("#data").attr("randomURL");
   $(".deleteResponse").click(function (event) {
     const endpoint = $(this).attr("url");
     const responseId = {
       id: $(this).attr("responseId")
     }
-    const randomURL = $(this).attr("randomURL");
 
     $.ajax({
       url: endpoint,
@@ -22,35 +22,35 @@ $(() => {
     })
   })
 
-  // $("#updatePoll").click(function (event) {
-	// 	event.preventDefault();
-	// 	const formVariables = {};
-	// 	formVariables["options"] = [];
-	// 	let temporaryInputArray = [];
-  //
-	// 	$("#responses .option :input").each(function(){
-	// 		temporaryInputArray.push($(this).val());
-	// 	});
-  //
-	// 	for(let i = 0; i < temporaryInputArray.length; i += 2) {
-	// 		let tempObj = {};
-	// 		tempObj["response"] = temporaryInputArray[i];
-	// 		tempObj["description"] = temporaryInputArray[i+1];
-	// 		formVariables.options.push(tempObj);
-	// 	}
-  //
-	// 	formVariables.question = $("#question").val();
-	// 	formVariables.name = $("#name").val();
-	// 	formVariables.email = $("#email").val();
-	// 	formVariables.end = new Date($("#end").val()).toISOString();
-  //
-	// 	$.ajax({
-	// 		url: "/polls/new",
-	// 		method: "POST",
-	// 		data: formVariables,
-	// 		dataType: "json"
-	// 	}).done(function(response) {
-	// 		window.location.href = "http://localhost:8080"+response.url;
-	// 	});
-	// });
+  $("#updatePoll").submit(function (event) {
+		event.preventDefault();
+		const formVariables = {};
+		formVariables["options"] = [];
+		let temporaryInputArray = [];
+
+		$("#responses .option :input").each(function(){
+			temporaryInputArray.push($(this).val());
+		});
+
+		for(let i = 0; i < temporaryInputArray.length-1; i += 2) {
+			let tempObj = {};
+			tempObj["response"] = temporaryInputArray[i];
+			tempObj["description"] = temporaryInputArray[i+1];
+			formVariables.options.push(tempObj);
+		}
+
+		formVariables.question = $("#question").val();
+		formVariables.name = $("#name").val();
+		formVariables.email = $("#email").val();
+		formVariables.end = new Date($("#end").val()).toISOString();
+
+		$.ajax({
+			url: `/polls/${randomURL}/update`,
+			method: "POST",
+			data: formVariables,
+			dataType: "json"
+		}).done(function(response) {
+			window.location.href = `http://localhost:8080/polls/${randomURL}/admin`;
+		});
+	});
 });
